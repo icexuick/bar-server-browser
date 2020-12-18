96e99e1904b232cd4e7c90677911c0493cbccf9a
Jazcash <jaspercashmore@gmail.com>
2020-12-18 14:00:58 +0000

    player/spectator icons

--- src/client/ts/components/battle.vue
+++ src/client/ts/components/battle.vue
@@ -3,18 +3,20 @@
         <div class="battle__background" style="--bg: url(images/maps/unknown.jpg)"></div>
         <div class="battle__background" :style="'--bg: url(images/maps/'+ mapUrl +'.jpg)'"></div>
         <div class="battle__top">
-            <div class="battle__title">
-                {{battle.title}}
-            </div>
-            <div class="battle__map">
-                {{mapName}}
-            </div>
+            <div class="battle__title">{{battle.title}}</div>
+            <div class="battle__map">{{mapName}}</div>
             <div v-if="battle.locked || battle.passworded" class="battle__lock">
                 <img src="images/lock.png">
             </div>
-            <div class="battle__player-count">
-                {{battle.players.length}} / {{battle.maxPlayers}}
+            <div v-if="playerCount" class="battle__player-count">
+                {{playerCount}}
+                <img src="images/player.png">
             </div>
+            <div v-if="spectatorCount" class="battle__spectator-count">
+                {{spectatorCount}}
+                <img src="images/spectator.png">
+            </div>
+            <!-- <div class="battle__max-players">/ {{battle.maxPlayers}}</div> -->
         </div>
         <div class="battle__players">
             <div class="battle__player" v-for="player in battle.players" v-bind:key="player.userId">
@@ -25,9 +27,7 @@
                     <img v-if="player.status" :src="'images/ranks/' + (player.status.rank + 1) + '.png'">
                     <img v-else src="images/ranks/1.png">
                 </div>
-                <div class="battle__username">
-                    {{player.username}}
-                </div>
+                <div class="battle__username">{{player.username}}</div>
                 <div class="battle__ingame">
                     <img v-if="player.status && player.status.ingame" src="images/ingame.png">
                     <img v-else src="images/battle.png">
@@ -56,6 +56,14 @@ export default Vue.extend({
         mapName: function() {
             const battle = this.battle as Battle;
             return battle.map.replace(/[\_\-]/g, " ");
+        },
+        playerCount: function() {
+            const battle = this.battle as Battle;
+            return battle.players.length - (battle.spectators - 1);
+        },
+        spectatorCount: function() {
+            const battle = this.battle as Battle;
+            return battle.spectators - 1;
         }
     }
 });
